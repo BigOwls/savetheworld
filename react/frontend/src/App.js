925c2ae3e890c69bfe384867edca0374834514f1
mcruz1101 <matheuscruz1101@gmail.com>
2019-11-29 02:14:08 -0300

    handleSubmit

--- react/frontend/src/App.js
+++ react/frontend/src/App.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api'
 import './App.css';
 
 import logo from './images/logoG.png';
 
 function App() {
+
+  const [email,setEmail] = useState('');
+
+  async function handleSubmit(event){
+    event.preventDefault();
+
+    const response = await api.post('/sessions', {email})
+
+    console.log(email)
+  }
   return (
     <div className='container'>  
       <img src={logo} alt='Teste'/>
 
       <div className='content'>
-        <p class='t1'>
+        <p className='t1'>
           Eco World
         </p>
 
-        <p class='t2'>
+        <p className='t2'>
           Encontre o Ponto mais Proximo
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor='email'>E-MAIL *</label>
           <input 
             type='email' 
             id='email'  
             placeholder='Digite seu e-mail'
+            value={email}
+            onChange={event => setEmail(event.target.value) }
           />
           <button className='btn' type='submit'>
             Entrar
---
+++ react/frontend/src/indexpoint.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppPoint from './AppPoint';
+
+ReactDOM.render(<AppPoint />, document.getElementById('root'));
